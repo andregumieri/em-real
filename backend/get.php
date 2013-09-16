d4f741a29ad44c39ccb61fe07bf3aa8b50bde7df
André Gumieri <andregumieri@gmail.com>
2013-09-16 15:31:14 -0300

    Evitar cache ao atualizar o dolar

--- backend/get.php
+++ backend/get.php
@@ -3,5 +3,8 @@ $from = $_GET['from'];
 $to = $_GET['to'];
 $json = file_get_contents("http://rate-exchange.appspot.com/currency?from={$from}&to={$to}");
 header("Content-type: application/json");
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
 echo $json;
 ?>
