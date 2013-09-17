55cd17c8ec158de5b7aef04401918409a43d8833
André Gumieri <andregumieri@gmail.com>
2013-09-17 11:46:01 -0300

    Mudança para um webservice que trás informações mais confiáveis

--- backend/get.php
+++ backend/get.php
@@ -1,10 +1,29 @@
 <?php
-$from = $_GET['from'];
-$to = $_GET['to'];
-$json = file_get_contents("http://rate-exchange.appspot.com/currency?from={$from}&to={$to}");
-header("Content-type: application/json");
-header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-header("Cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache");
-echo $json;
+	/** 
+	 * Converte moedas
+	 * @link http://www.webservicex.net/ws/WSDetails.aspx?CATID=2&WSID=10
+	 *
+	 * @param STRING $from - Moeda de origem
+	 * @param STRING $to - Moeda de destino
+	 * 
+	 * @return JSON - DOUBLE rate, STRING from, STRING to
+	 */
+
+	// Parametros
+	$from = $_GET['from'];
+	$to = $_GET['to'];
+
+	// Pega resultados
+	$xml = file_get_contents("http://www.webservicex.net/CurrencyConvertor.asmx/ConversionRate?FromCurrency={$from}&ToCurrency={$to}");
+
+	// Parse do resultado
+	$xmlget = simplexml_load_string($xml);
+	$rate = floatval($xmlget[0]);
+
+	// Exibe resultados
+	header("Content-type: application/json");
+	header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+	header("Cache-Control: post-check=0, pre-check=0", false);
+	header("Pragma: no-cache");
+	echo json_encode(array('to'=>$to, 'from'=>$from, 'rate'=>$rate));
 ?>
